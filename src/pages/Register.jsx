617d2f7f191c2a6dd9f44ecb36475a56faf56137
Mehmetukaj Agonis <agonis_98@hotmail.com>
2022-03-09 19:45:09 +0100

    Register-Page weiter entwickelt

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,47 @@
+import { useState, useEffect } from 'react'
+import {FaUser} from 'react-icons/fa'
+
+
 function Register() {
+  const [formData, setForm] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  })
+
+  const onChange = () => {
+
+  }
+
+  const { name, email, password, password2 } = formData
+
   return (
-    <div>Register</div>
+    <>
+      <section className='heading'>
+        <h1>
+          <FaUser /> Registrierung
+        </h1>
+        <p>Bitte erstellen Sie ein neuen Account</p>
+      </section>
+
+      <section className="form">
+        <form>
+          <div className="form-group">
+            <input type="text" className="form-control" id="name" name='name' value={name} placeholder='Bitte Namen eingeben' onChange={onChange} />
+          </div>
+          <div className="form-group">
+            <input type="text" className="form-control" id="email" name='email' value={name} placeholder='Bitte Email eingeben' onChange={onChange} />
+          </div>
+          <div className="form-group">
+            <input type="password" className="form-control" id="password" name='password' value={name} placeholder='Bitte Passwort eingeben' onChange={onChange} />
+          </div>
+          <div className="form-group">
+            <input type="passowrd" className="form-control" id="password2" name='password2' value={name} placeholder='Passwort bestätigen' onChange={onChange} />
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 

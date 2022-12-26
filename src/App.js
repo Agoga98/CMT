11d956fb279660682e5f4f8518959d2c2dbcd1a1
Blerine <binejaha@gmail.com>
2022-12-26 22:57:12 +0100

    Nachm Merge

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-<<<<<<< HEAD
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import Header from "./components/Header";
-import Dashboard from "./pages/Dashboard";
-import Login from "./pages/Login";
-import CustomerOverview from "./pages/CustomerOverview";
-import Register from "./pages/Register";
-import Settings from "./pages/Settings";
-import Statistics from "./pages/Statistics";
-=======
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import BauvorhabenAnlegen from './components/BauvorhabenContainer/BauvorhabenAnlegen';
 import Header from './components/Header';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import CustomerOverview from './pages/CustomerOverview';
-import NewCustomer from './pages/NewCustomer';
 import Register from './pages/Register';
 import Settings from './pages/Settings';
 import Statistics from './pages/Statistics';
->>>>>>> Blerine
 
 const App = () => {
   return (
@@ -50,29 +35,4 @@ const App = () => {
   );
 };
 
-<<<<<<< HEAD
 export default App;
-=======
-    return( 
-        <>
-            <Router>
-                <div className='container'>
-                    <Header/>
-                    <Routes>
-                        <Route path='/' element={<Dashboard/>}/>
-                        <Route path='/login' element={<Login/>}/>
-                        <Route path='/register' element={<Register/>}/>
-                        <Route path='/customeroverview' element={<CustomerOverview customerID={"622b3da245549725c3463026"}/>}/>
-                        <Route path='/settings' element={<Settings/>}/>
-                        <Route path='/statistics' element={<Statistics/>}/>
-                        <Route path='/newproject' element={<BauvorhabenAnlegen/>}/>
-                    </Routes>
-                </div> 
-            </Router>
-            <ToastContainer/>
-        </>
-    )
-}
-
-export default App;
->>>>>>> Blerine

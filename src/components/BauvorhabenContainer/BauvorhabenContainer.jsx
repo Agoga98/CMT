5689e762cf5e2a763c76d99c952eeb065a81e7b0
Mehmetukaj Agonis <agonis_98@hotmail.com>
2023-01-22 11:35:43 +0100

    Keine wircklichen Änderungen

--- src/components/BauvorhabenContainer/BauvorhabenContainer.jsx
+++ src/components/BauvorhabenContainer/BauvorhabenContainer.jsx
@@ -1,29 +1,28 @@
-import { Link} from "react-router-dom"
-import React, {useEffect, navigate} from "react"
-import BauvorhabenProjekt from "./BauvorhabenProjekt"
-import  {useSelector, useDispatch} from "react-redux"
-import {getProjects} from "../../features/project/projectSlice"
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-
+import { Link, useNavigate } from "react-router-dom";
+import React, { useEffect, navigate } from "react";
+import BauvorhabenProjekt from "./BauvorhabenProjekt";
+import { useSelector, useDispatch } from "react-redux";
+import { getProjects } from "../../features/project/projectSlice";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 function BauvorhabenContainer() {
-  const {user} = useSelector((state) => state.auth)
-  const {projects} = useSelector((state)=>{
-    return state.projects
-  })
-  const dispatch=useDispatch()
-  
+  const { user } = useSelector((state) => state.auth);
+  const { projects } = useSelector((state) => {
+    return state.projects;
+  });
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   useEffect(() => {
-    if(!user)
-    {
-      navigate('/login')
+    if (!user) {
+      navigate("/login");
     }
 
-    dispatch(getProjects())
-  }, [user])
+    dispatch(getProjects());
+  }, [user]);
 
-  
   return (
     <>
       <div className="customer-bvh-box-nav">
@@ -32,34 +31,44 @@ function BauvorhabenContainer() {
             <Link className="btn-link " to="/newproject">
               <button className="btn">Neues Projekt</button>
             </Link>
-        </Nav>
-        <Nav class="dropdown">
-    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown" id="dropdownfilter">Filter
-    <span class="caret"></span></button>
-    <div class="dropdown-menu" aria-labelledby="dropdownfilter">
-      <input class="text" id="myInput" type="text" placeholder="Search.."/>
-      <option type="checkbox" aria-labelledby="myInput" >HTML</option>
-           
-    </div>
-  </Nav>
+          </Nav>
+          <Nav class="dropdown">
+            <button
+              class="btn btn-primary dropdown-toggle"
+              type="button"
+              data-toggle="dropdown"
+              id="dropdownfilter"
+            >
+              Filter
+              <span class="caret"></span>
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownfilter">
+              <input
+                class="text"
+                id="myInput"
+                type="text"
+                placeholder="Search.."
+              />
+              <option type="checkbox" aria-labelledby="myInput">
+                HTML
+              </option>
+            </div>
+          </Nav>
         </Navbar>
-        </div>
-        <div className="all-bvh-containers">
+      </div>
+      <div className="all-bvh-containers">
         <div className="bvh-cards-box">
-       {
-        projects.map((val)=>{
-          return (
-          <> 
-          <BauvorhabenProjekt project={val}/>
-          </>
-          )
-        })
-       } 
+          {projects.map((val) => {
+            return (
+              <>
+                <BauvorhabenProjekt project={val} />
+              </>
+            );
+          })}
+        </div>
       </div>
-      </div>
-      </>
-   
-  )
+    </>
+  );
 }
 
-export default BauvorhabenContainer
+export default BauvorhabenContainer;
